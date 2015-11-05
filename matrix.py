f3ba48a7d099dd56df5f22b115f6307ab0c1efb3
Cuong Dong <cuong.dong@gmail.com>
2015-11-05 09:43:22 -0800

    Use ASCII instead of Chinese characters.
    Chinese characters are bigger than ASCII and seem to cause problem. Look into it later.
    Tune the falling speed and sleep time between frame.

--- matrix.py
+++ matrix.py
@@ -4,11 +4,19 @@ import random
 import curses
 import time
 
+#Sleep between frame after refresh so that user can see the frame. Value 0.01 or lower results in flickering because the
+# animation is too fast.
+SLEEP_BETWEEN_FRAME = .03
+
+#How fast the rain should fall
+FALLING_SPEED = 2
+
 def random_rain_length():
     return random.randint(curses.LINES, 3*curses.LINES//2)
 
 def random_char():
-    return chr(random.randint(0x4E00, 0x62FF))
+    # return chr(random.randint(0x4E00, 0x62FF))
+    return chr(random.randint(ord('A'), ord('z')))
 
 def animate_rain(stdscr, x):
     max_length = random_rain_length()
@@ -22,9 +30,7 @@ def animate_rain(stdscr, x):
         if (middle < 0):
             middle = 0
 
-        for i in range(0, tail):
-            stdscr.addstr(i, x, ' ')
-        if (tail == curses.LINES):
+        if (tail >= curses.LINES):
             break
 
         for i in range(tail, min(middle, curses.LINES)):
@@ -36,7 +42,7 @@ def animate_rain(stdscr, x):
         if (head < curses.LINES - 1):
             stdscr.addstr(head, x, random_char())
 
-        head = head + 1
+        head = head + FALLING_SPEED
         yield
 
 
@@ -46,8 +52,9 @@ def main(stdscr):
     stdscr.clear()
 
     stdscr.nodelay(True)
-    rains = [animate_rain(stdscr, random.randrange(curses.COLS)) for i in range(50)]
+    rains = [animate_rain(stdscr, random.randrange(curses.COLS - 2)) for i in range(curses.COLS//2)]
     while True:
+        stdscr.clear()
         for r in rains:
             try:
                 next(r)
@@ -55,13 +62,14 @@ def main(stdscr):
                 rains.remove(r)
 
         stdscr.refresh()
-        time.sleep(.1)
+
+
 
         # ch = stdscr.getch()
         # if (ch == curses.KEY_ENTER):
         #     break
         if not rains:
             break
-
+        time.sleep(SLEEP_BETWEEN_FRAME)
 
 curses.wrapper(main)
