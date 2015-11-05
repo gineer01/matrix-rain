c0d255f56d3f6c4af1119b102e9ea8d165a457ab
Cuong Dong <cuong.dong@gmail.com>
2015-11-04 16:22:49 -0800

    Simplify and show rain with head, body, and tail - Simplify rain to just fall along x-axis - show rain with head, body, and tail - remove rain from queue if finished and exit if queue is empty

--- matrix.py
+++ matrix.py
@@ -5,35 +5,62 @@ import curses
 import time
 
 def random_rain_length():
-    return random.randint(curses.LINES/2, curses.LINES)
+    return random.randint(curses.LINES, 3*curses.LINES//2)
 
 def random_char():
     return chr(random.randint(0x4E00, 0x62FF))
 
-def animate_rain(stdscr, x, y):
-    length = random_rain_length()
-    stdscr.addstr(y, x, "{} {} {}".format(x, y, length))
+def animate_rain(stdscr, x):
+    max_length = random_rain_length()
+    head = 1
 
-    done = False
-    while not done:
-        for i in range(y, length):
+    while True:
+        tail = head - max_length
+        if (tail < 0):
+            tail = 0
+        middle = head - max_length//2
+        if (middle < 0):
+            middle = 0
+
+        for i in range(0, tail):
+            stdscr.addstr(i, x, ' ')
+        if (tail == curses.LINES):
+            break
+
+        for i in range(tail, min(middle, curses.LINES)):
+            stdscr.addstr(i, x, random_char())
+
+        for i in range(middle, min(head - 1, curses.LINES)):
             stdscr.addstr(i, x, random_char(), curses.A_REVERSE)
+
+        if (head < curses.LINES):
+            stdscr.addstr(head, x, random_char())
+
+        head = head + 1
         yield
 
 
 def main(stdscr):
     # Clear screen
+    curses.curs_set(0)
     stdscr.clear()
 
     stdscr.nodelay(True)
-    rains = [animate_rain(stdscr, random.randrange(curses.COLS - 10), random.randrange(curses.LINES/2)) for i in range(50)]
+    rains = [animate_rain(stdscr, random.randrange(curses.COLS)) for i in range(50)]
     while True:
         for r in rains:
-            next(r)
+            try:
+                next(r)
+            except StopIteration:
+                rains.remove(r)
 
-        time.sleep(.01)
-        ch = stdscr.getch()
-        if (ch == curses.KEY_ENTER):
+        stdscr.refresh()
+        time.sleep(.1)
+
+        # ch = stdscr.getch()
+        # if (ch == curses.KEY_ENTER):
+        #     break
+        if not rains:
             break
 
 
